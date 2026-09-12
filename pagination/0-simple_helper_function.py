1d4b54c8fa337adc1144ac4f12b28e0554ca4dd7
iEmanHamdan <eman.hamdan@outlook.sa>
2026-09-12 23:07:13 +0300

    Fix docstring and annotations for task 0 pagination helper

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """
-Simple helper function module for pagination.
+Module for simple pagination helper function.
 """
-from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> tuple:
     """
-    Calculate start and end indexes for pagination parameters.
+    Return a tuple of size two containing a start index and
+    an end index for the given pagination parameters.
     """
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
-    
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
