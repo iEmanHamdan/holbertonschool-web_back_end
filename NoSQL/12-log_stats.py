cb6ba02b18b45f7ccaf574c9d76e05fae9a5d1cc
iEmanHamdan <eman.hamdan@outlook.sa>
2026-09-12 22:12:27 +0300

    Refactor 12-log_stats.py for improved docstring clarity and structure

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 """
-Provides some stats about Nginx logs stored in MongoDB.
+Python script that provides some stats about Nginx logs stored in MongoDB.
 """
 from pymongo import MongoClient
 
 
-def log_stats() -> None:
-    """
-    Free function that displays statistics about Nginx logs
-    stored in the 'logs' database, 'nginx' collection.
-    """
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+    db = client.logs
+    nginx_collection = db.nginx
 
     total_logs = nginx_collection.count_documents({})
     print(f"{total_logs} logs")
@@ -26,7 +23,4 @@ def log_stats() -> None:
         {"method": "GET", "path": "/status"}
     )
     print(f"{status_checks} status check")
-
-
-if __name__ == "__main__":
-    log_stats()
+    
