aa09b1b4798d9f80885eb1657bd21dd4d7e698f9
iEmanHamdan <eman.hamdan@outlook.sa>
2026-09-12 23:20:24 +0300

    Refactor 1-simple_pagination.py for improved docstring clarity and consistency

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python3
-"""
-Simple pagination module.
-"""
+"""Simple pagination."""
+
 import csv
-import math
-from typing import List, Tuple
+from typing import List
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Calculate start and end indexes for pagination parameters.
-    """
-    return ((page - 1) * page_size, page * page_size)
+def index_range(page: int, page_size: int) -> tuple:
+    """Return the index range for a page."""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Return the cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -33,18 +30,14 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Get a specific page of data from dataset.
-        """
+    def get_page(
+            self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return a page of the dataset."""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
+        dataset = self.dataset()
         start, end = index_range(page, page_size)
-        data = self.dataset()
 
-        if start >= len(data):
-            return []
-
-        return data[start:end]
+        return dataset[start:end]
     
